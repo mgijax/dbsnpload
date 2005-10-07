eb8ab44129f91068c87b50cdf144223890c5b5ae
Sharon Giannatto <Sharon.Giannatto@jax.org>
2005-10-07 12:49:55 +0000

    removed renaming of old vocab files, added -v, -f parameter

--- bin/loadVoc.sh
+++ bin/loadVoc.sh
@@ -1,8 +1,39 @@
-#!/bin/sh
+#!/bin/sh 
+#
+#   $Header
+#   $Name
+# loadVoc.sh
+##############################################################################
+# Purpose: Load SNP Vocabularies
+#
+Usage="loadVoc.sh [-f -v -h]"
+# where:
+#	-f run the fxnClass vocload
+#	-v run the varClass vocload
+#	-h run the subHandle vocload
+#  Env Vars:  None
+#
+#  Inputs:
+#
+#      	- Configuration files
+#      	- Configured vocab input file
+#  Outputs:
+#	- logs
+#	- bcp files
+#	- VOC_Term and VOC_Vocab records in a database
+#  Exit Codes:
+#	0 = Successful completion
+#	1 = An error occurred
+#	2 = Usage error occured
+#  Assumes:
+#	 - DLA standards are being followed for environment variable name
+#	 - all config files are in the same directory as this script
+#  Implementation:
+#
+#  Notes:  None
+#
+###########################################################################
 
-#
-# Load SNP VOC vocabularies
-#
 cd `dirname $0`/..
 LOG=`pwd`/loadVoc.log
 rm -f ${LOG}
@@ -12,14 +43,29 @@ date | tee -a ${LOG}
 #
 #  Verify the argument(s) to the shell script.
 #
-if [ $# -ne 0 ]
+doFxn=no
+doVar=no
+doHandle=no
+
+set -- `getopt fvh $*`
+if [ $? != 0 ]
 then
-    echo ${Usage} | tee -a ${LOG}
-    exit 1
+    echo ${Usage}
+    exit 2
 fi
 
+for i in $*
+do
+    case $i in
+        -f) doFxn=yes; shift;;
+        -v) doVar=yes; shift;;
+	-h) doHandle=yes; shift;;
+        --) shift; break;;
+    esac
+done
+
 #
-#  Establish general the configuration file names
+#  Establish general configuration file names
 #
 CONFIG_COMMON=`pwd`/common.config.sh
 CONFIG_LOAD=`pwd`/dbsnpload.config
@@ -28,10 +74,9 @@ CONFIG_LOAD=`pwd`/dbsnpload.config
 #  Establish vocload config file names
 #
 
-FXNCLASS_VOCAB_CONFIG=`pwd`/fxnClass.config
-VARCLASS_VOCAB_CONFIG=`pwd`/varClass.config
-HANDLE_VOCAB_CONFIG=`pwd`/subHandle.config
-
+FXNCLASS_VOCAB_CONFIG=`pwd`/fxnClassVocab.config
+VARCLASS_VOCAB_CONFIG=`pwd`/varClassVocab.config
+HANDLE_VOCAB_CONFIG=`pwd`/subHandleVocab.config
 #
 #  Make sure the configuration files are readable.
 #
@@ -66,11 +111,14 @@ then
 fi
 
 #
-# Source configuration files
+# Source general configuration files
 #
 . ${CONFIG_COMMON}
 . ${CONFIG_LOAD}
 
+#
+# function that reports status given a status ($1) and a process name ($2)
+#
 checkstatus ()
 {
 
@@ -87,55 +135,54 @@ checkstatus ()
 # main
 #
 
-echo ${LOG_DIAG}
 date | tee -a ${LOG_DIAG}
 
-echo "Creating fxnClass vocab..." | tee -a ${LOG} ${LOG_DIAG}
-${VOCSIMPLELOAD} ${FXNCLASS_VOCAB_CONFIG} >> ${LOG_DIAG} 2>&1
+if [ ${doFxn} = "yes" ]
+then
+    echo "Creating fxnClass vocab..." | tee -a ${LOG} ${LOG_DIAG}
+    ${VOCSIMPLELOAD} ${FXNCLASS_VOCAB_CONFIG}
 
-## DEBUG ##
-#. /home/sc/snp/vocload/Configuration
-#DAG_ROOT_ID=""
-#export DAG_ROOT_ID
-#. /home/sc/snp/dbsnpload/fxnClass.config
+    ## DEBUG ##
+    #. /home/sc/snp/vocload/Configuration
+    #DAG_ROOT_ID=""
+    #export DAG_ROOT_ID
+    #. /home/sc/snp/dbsnpload/fxnClassVocab.config
 
-#/home/sc/snp/vocload/simpleLoad.py -f -l testLog.txt /home/sc/snp/vocload/simple.rcd /home/sc/snp/dbsnpload/data/fxnClass.in
-## END DEBUG ##
+    #/home/sc/snp/vocload/simpleLoad.py -f -l testLog.txt /home/sc/snp/vocload/simple.rcd /home/sc/snp/dbsnpload/data/fxnClass.in
+    ## END DEBUG ##
 
-STAT=$?
-msg="fxnClass vocab load"
-checkstatus  ${STAT} ${msg}
+    STAT=$?
+    msg="fxnClass vocab load"
+    checkstatus  ${STAT} ${msg}
+fi
 
-echo "Creating varClass vocab..." | tee -a ${LOG} ${LOG_DIAG}
-${VOCSIMPLELOAD} ${VARCLASS_VOCAB_CONFIG} >> ${LOG_DIAG}  2>&1
-STAT=$?
-msg="varClass vocab load"
-checkstatus  ${STAT} ${msg}
+if [ ${doVar} = "yes" ]
+then
+    echo "Creating varClass vocab..." | tee -a ${LOG} ${LOG_DIAG}
+    ${VOCSIMPLELOAD} ${VARCLASS_VOCAB_CONFIG}
+    STAT=$?
+    msg="varClass vocab load"
+    checkstatus  ${STAT} ${msg}
+fi
 
-#
-# rename old vocab files
-#
-echo "Renaming ${HANDLE_VOCAB_FILE} to ${HANDLE_VOCAB_FILE}.old"
-echo "Renaming ${INT_HANDLE_VOCAB_FILE} to ${INT_HANDLE_VOCAB_FILE}.old"
-/usr/bin/mv ${HANDLE_VOCAB_FILE} ${HANDLE_VOCAB_FILE}.old
-/usr/bin/mv ${INT_HANDLE_VOCAB_FILE} ${INT_HANDLE_VOCAB_FILE}.old
+if [ ${doHandle} = "yes" ]
+then
+    echo "Creating subHandle vocab input file..." | tee -a ${LOG} ${LOG_DIAG}
+    # transforms: <NSE-ss_handle>WI</NSE-ss_handle>
+    # into: WI
+    /usr/bin/cat ${NSE_SNP_INFILEDIR}/*.xml | grep "<NSE-ss_handle>" | cut -d'>' -f2 | cut -d'<' -f1 | sort | uniq > ${INT_HANDLE_VOCAB_FILE}
 
-echo "Creating subHandle vocab input file..." | tee -a ${LOG} ${LOG_DIAG}
-# transforms: <NSE-ss_handle>WI</NSE-ss_handle>
-# into: WI
-/usr/bin/cat ${NSE_SNP_INFILEDIR}/*.xml | grep "<NSE-ss_handle>" | cut -d'>' -f2 | cut -d'<' -f1 | sort | uniq > ${INT_HANDLE_VOCAB_FILE}
+    # creates subHandle vocab input file which will create
+    # accession records for the vocab 
+    # file looks like:
+    # WI tab WI
+    # where WI is the term name AND the accession id
+    ${HANDLE_VOCAB_FILE_CREATOR}
 
-# creates subHandle vocab input file which will create
-# accession records for the vocab 
-# file looks like:
-# WI tab WI
-# where WI is the term name AND the accession id
-${HANDLE_VOCAB_FILE_CREATOR}
-
-echo "Creating subHandle vocab ..." | tee -a ${LOG} ${LOG_DIAG}
-${VOCSIMPLELOAD} ${HANDLE_VOCAB_CONFIG} >> ${LOG_DIAG} 2>&1
-STAT=$?
-msg="subHandle vocab load"
-checkstatus  ${STAT} ${msg}
-
+    echo "Creating subHandle vocab ..." | tee -a ${LOG} ${LOG_DIAG}
+    ${VOCSIMPLELOAD} ${HANDLE_VOCAB_CONFIG}
+    STAT=$?
+    msg="subHandle vocab load"
+    checkstatus  ${STAT} ${msg}
+fi
 date | tee -a ${LOG} ${LOG_DIAG}
