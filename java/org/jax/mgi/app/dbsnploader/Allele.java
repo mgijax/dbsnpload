80c4e42c3bfa4204a266601ff18700c857d2efce
Sharon Giannatto <Sharon.Giannatto@jax.org>
2005-11-28 14:47:21 +0000

    removed gtyFlag1 attribute, not using it

--- java/org/jax/mgi/app/dbsnploader/Allele.java
+++ java/org/jax/mgi/app/dbsnploader/Allele.java
@@ -3,8 +3,6 @@ package org.jax.mgi.app.dbsnploader;
 class Allele {
     // The snp typing for this strain e.g. "T", "-", "ACTG"
     private String allele;
-    // dbSNP flag conflict flag; not using this
-    private boolean gtyFlag1 = false;
     // the orientation of this allele with respect to the RS flanking sequence
     private String orient;
     //default constructor
@@ -19,17 +17,11 @@ class Allele {
     public void setAllele (String all) {
         allele = all;
     }
-    public void setGtyFlag1 (boolean f) {
-        gtyFlag1 = f;
-    }
     public void setOrientation(String o) {
         orient = o;
     }
     public String getAllele() {
         return allele;
-    }
-    public boolean getGtyFlag1() {
-        return gtyFlag1;
     }
     public String getOrientation() {
         return orient;
