74bcca3f9f9776f0b9b3d333ba0b2f9f0f849b0e
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-01-10 19:19:04 +0000

    added deleteStrainSet method and comments

--- java/org/jax/mgi/app/dbsnploader/DBSNPLoader.java
+++ java/org/jax/mgi/app/dbsnploader/DBSNPLoader.java
@@ -28,53 +28,73 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/**
+ * is an object that parses DBSNP input files, resolves dbsnp values
+ * to MGI values, and loads DBSNPs into a database
+ * @has
+ * radar and mgd streams to create bcp files and database records
+ * DBSNPProcessor to resolve attributes and create radar and mgd DAOs
+ * BufferedWriter to write coordinate feature information to a file
+ * A set of chromosomes to determine which files to process
+ * @does parses DBSNP input files, resolves DBSNP attributes to MGI values
+ * and creates radar and mgd database objects
+ * @company Jackson Laboratory
+ * @author sc
+ *
+ */
+
 public class DBSNPLoader extends DLALoader {
 
-    // the SQLStream used for loading data
+    // the SQLStream used for loading radar data
     private SQLStream radarStream;
 
-    // Puts SNPs into the database
+    // Resolves DBSNP attributes to MGI values and writes radar and mgd
+    // bcp files
     private DBSNPInputProcessor dbsnpProcessor;
 
-    // current number of SNPs processed
+    // current number of RefSNPs processed
     private int snpCtr;
 
-    // current number of rs with alleles loaded into memory
+    // current number of RefSNPs with no strain alleles
     private int rsWithNoAllelesCtr;
 
-    // current number of rs with no BL6 coordinates
+    // current number of RefSNPs with no BL6 coordinates
     private int rsWithNoBL6Ctr;
 
-    // current number of rs with multiple BL6 chromosomes
+    // current number of RefSNPs with multiple BL6 chromosomes
     private int rsMultiBL6ChrCtr;
 
-    // current number of rs with no allele summary (means no strain resolved or
-    // all alleles are 'N'
+    // current number of RefSNPs with no allele summary
+    // (means no strain resolved or all alleles are 'N' or ' ')
     private int rsWithNoAlleleSummaryCtr;
 
-    // current number of rs with unresolvable vocabularies
+    // current number of RefSNPs with unresolvable vocabularies
     private int rsWithVocabResolverExceptionCtr;
 
-    // current number of rs repeated in input
+    // current number of RefSNPs repeated in input
     private int rsRepeatExceptionCtr;
 
-    // list of chromosomes to parse
+    // list of chromosomes for which we want to parse chromosome files
     private ArrayList chrList;
 
     // writer for CoordLoad input file
     private BufferedWriter coordWriter;
 
-    //load configurator
+    // load configurator
     private DBSNPLoaderCfg loadCfg;
+
     // configurator for genotype file configuration
     private DBSNPLoaderCfg genoConfig;
+
     // configurator for nse file configuration
     private DBSNPLoaderCfg nseConfig;;
+
     // SNP exception factory
     private SNPLoaderExceptionFactory snpEFactory;
 
     // file path separator
-    private static final String PATH_SEPARATOR = System.getProperty("file.separator");;
+    private static final String PATH_SEPARATOR = System.getProperty("file.separator");
+
     /**
      * Initializes instance variables
      * @effects instance variables will be instantiated
@@ -83,6 +103,7 @@ public class DBSNPLoader extends DLALoader {
     protected void initialize() throws MGIException {
         // configurator for the load
         loadCfg = new DBSNPLoaderCfg();
+
         // configurators for each input file
         genoConfig = new DBSNPLoaderCfg("GENO");
         nseConfig = new DBSNPLoaderCfg("NSE");
@@ -104,10 +125,15 @@ public class DBSNPLoader extends DLALoader {
             throw new MGIException(e.getMessage());
         }
 
-        // rename the stream for clarity - the DLA thinks of the radar database as qc.
+        // rename the stream for clarity -
+	// the DLA thinks of the radar database as qc
         radarStream = qcStream;
+
+	// create processor
         dbsnpProcessor = new DBSNPInputProcessor(radarStream, loadStream,
                                                  coordWriter);
+
+	// create ExceptionFactory
         snpEFactory = new SNPLoaderExceptionFactory();
 
 
@@ -124,14 +150,19 @@ public class DBSNPLoader extends DLALoader {
 
     /**
      * to perform load pre processing
-     * @effects any preprocessing will be performed
+     * @effects depending on configuration, SNP accessions and/or
+     * the snp strain set is deleted
      * @throws MGIException if errors occur during preprocessing
      */
     protected void preprocess() throws MGIException {
-        // delete accession records, note that truncating tables is done at the
-        // dla level via Configuration
+        // delete accession records, note that truncating SNP tables is done at
+        // the dla level via Configuration
         if(loadCfg.getOkToDeleteAccessions().equals(Boolean.TRUE)) {
             deleteAccessions();
+        }
+	// delete DBSNP Strain Set
+        if(loadCfg.getOkToDeleteStrainSet().equals(Boolean.TRUE)) {
+            deleteStrainSet();
         }
     }
 
@@ -148,51 +179,74 @@ public class DBSNPLoader extends DLALoader {
          * process chromosome files one at a time
          */
         for (Iterator i = chrList.iterator(); i.hasNext(); ) {
+            // reinitialize DBSNPProcessor structures for this chromosome
+            dbsnpProcessor.reinitializeProcessor();
+
             // encourage the garbage collector
             System.gc();
 
+	    // get the next chromosome
             String chr = ((String)i.next()).trim();
-            logger.logdInfo("Processing chr " + chr, true);
-            /**
-             * process the genotype file for the Individual data
-             */
+            logger.logdDebug("Processing chr " + chr + ". Free memory: " +  Runtime.getRuntime().freeMemory());
+
+            // create the genotype filename for this chromosome
             String genotypeFilename = genoConfig.getInfileDir()  +
                 PATH_SEPARATOR +
                 genoConfig.getInfilePrefix() + chr +
                 genoConfig.getInfileSuffix();
+            /**
+             * process the genotype file for Individual data
+             */
+            logger.logdDebug("processing genotype file for Individual data " +
+			    genotypeFilename + ". Free memory: " +
+			    Runtime.getRuntime().freeMemory());
             XMLDataIterator indivIterator =
-                new DBSNPGenotypeIndividualInputFile(genotypeFilename).getIterator();
+                new DBSNPGenotypeIndividualInputFile(
+				genotypeFilename).getIterator();
             while(indivIterator.hasNext() ) {
-                //DBSNPInput indivInput = (DBSNPInput)indivIterator.next();
-                dbsnpProcessor.processGenoIndivInput((DBSNPGenotypeIndividualInput)indivIterator.next());
+                dbsnpProcessor.processGenoIndivInput(
+				(DBSNPGenotypeIndividualInput)indivIterator.
+				next());
             }
             /**
              * process the genotype file for RefSNP data
              */
-            System.out.println("processing genotype file " + genotypeFilename);
-            XMLDataIterator genoRefSNPIterator = new DBSNPGenotypeRefSNPInputFile(
-                 genotypeFilename, dbsnpProcessor.getIndividualMap()).getIterator();
+
+            logger.logdDebug("processing genotype file for RefSnp data " +
+			    genotypeFilename + ". Free memory: " +
+			    Runtime.getRuntime().freeMemory());
+            XMLDataIterator genoRefSNPIterator =
+		    new DBSNPGenotypeRefSNPInputFile(
+                 genotypeFilename, dbsnpProcessor.getIndividualMap()).
+		    getIterator();
             while (genoRefSNPIterator.hasNext()) {
-                dbsnpProcessor.processGenoRefSNPInput((DBSNPGenotypeRefSNPInput) genoRefSNPIterator.next());
+                dbsnpProcessor.processGenoRefSNPInput((DBSNPGenotypeRefSNPInput)
+				genoRefSNPIterator.next());
             }
 
-            /**
-             * process the NSE file
-             */
+            // create the nse filename for this chromosome
             String nseFilename = nseConfig.getInfileDir()  +
                 PATH_SEPARATOR +
                 nseConfig.getInfilePrefix() + chr +
                 nseConfig.getInfileSuffix();
+
+            /**
+             * process the NSE file
+             */
             System.out.println("processing NSE file " + nseFilename);
-            XMLDataIterator it = new DBSNPNseInputFile(nseFilename).getIterator();
+            XMLDataIterator it = new DBSNPNseInputFile(nseFilename).
+		    getIterator();
             while (it.hasNext()) {
                 DBSNPNseInput nseInput = (DBSNPNseInput)it.next();
+                //logger.logdDebug("Processing " + nseInput.getRS().getRsId() +
+		//". Free memory: " +  Runtime.getRuntime().freeMemory(), true);
+
                 try {
-                    logger.logdDebug("Processing " + nseInput.getRS().getRsId());
                     dbsnpProcessor.processInput(nseInput);
                 }
                 catch (SNPNoStrainAlleleException e) {
-                   // logger.logdInfo("No StrainAlleles: " + input.getRsId(), true);
+                   // logger.logdInfo("No StrainAlleles: " +
+		   // input.getRsId(), true);
                     rsWithNoAllelesCtr++;
                 }
                 catch (SNPNoBL6Exception e) {
@@ -217,12 +271,11 @@ public class DBSNPLoader extends DLALoader {
 
 
     /**
-     * closes the load stream. Reports load statistics.
+     * closes the load stream and reports load statistics.
      * @throws MGIException
      */
     protected void postprocess() throws MGIException
     {
-
         try {
             coordWriter.close();
         } catch (IOException e) {
@@ -236,11 +289,7 @@ public class DBSNPLoader extends DLALoader {
     }
 
     /**
-     * Reports load statistics; event counts, organism counts, valid sequence count
-     * etc.
-     * @assumes nothing
-     * @effects nothing
-     * @throws Nothing
+     * Reports load statistics
      */
     private void reportLoadStatistics() {
         logger.logdInfo("Total RefSnps Looked at: " + snpCtr, false);
@@ -254,12 +303,19 @@ public class DBSNPLoader extends DLALoader {
                         rsWithNoAlleleSummaryCtr, false);
         logger.logdInfo("Total RefSnps with Vocab resolving errors: " +
                         rsWithVocabResolverExceptionCtr, false);
-        logger.logdInfo("Total RefSnp records repeated in the input (can be multiple per RefSnp): " +
+        logger.logdInfo("Total RefSnp records repeated in the input " +
+			"(can be multiple per RefSnp): " +
                         rsRepeatExceptionCtr, false);
-        for (Iterator i = dbsnpProcessor.getProcessedReport().iterator(); i.hasNext(); ) {
+        for (Iterator i = dbsnpProcessor.getProcessedReport().iterator();
+			i.hasNext(); ) {
             logger.logdInfo((String)i.next(), false);
         }
     }
+
+    /**
+     * deletes SNP Accessions, RefSNP, SubSNP and SubmitterSnp ids
+     * @throws MGIException
+     */
     private void deleteAccessions() throws MGIException {
         logger.logdInfo("Deleting Accessions", true);
 
@@ -279,9 +335,11 @@ public class DBSNPLoader extends DLALoader {
                     "select a._Accession_key " +
                     "from ACC_Accession a " +
                     "where a._MGIType_key =  " + MGITypeConstants.SUBSNP +
-                    " and a._LogicalDB_key = " + LogicalDBConstants.SUBMITTERSNP);
+                    " and a._LogicalDB_key = " +
+		    LogicalDBConstants.SUBMITTERSNP);
 
-            loadDBMgr.executeUpdate("create index idx1 on #todelete(_Accession_key)");
+            loadDBMgr.executeUpdate(
+			    "create index idx1 on #todelete(_Accession_key)");
 
             loadDBMgr.executeUpdate("delete ACC_Accession " +
                            "from #todelete d, ACC_Accession a " +
@@ -292,7 +350,35 @@ public class DBSNPLoader extends DLALoader {
                    (SNPLoaderException) snpEFactory.getException(
                    SNPLoaderExceptionFactory.SNPDeleteAccessionsErr, e);
                throw e1;
-
        }
+    }
+
+     /**
+     * deletes the DBSNP Strain Set
+     * @throws MGIException
+     */
+    private void deleteStrainSet() throws MGIException {
+        logger.logdInfo("Deleting DBSNP Strain Set", true);
+
+        try {
+            loadDBMgr.executeUpdate(
+                "select sm._SetMember_key " +
+                "into #membersTodelete " +
+                "from MGI_SetMember sm, MGI_Set s " +
+                "where sm._Set_key = s._Set_key " +
+                "and s.name = 'SNP Strains'"
+                );
+            loadDBMgr.executeUpdate(
+                "create index idx1 on #membersTodelete(_SetMember_key)");
+            loadDBMgr.executeUpdate("delete MGI_SetMember " +
+                                    "from #membersTodelete d, MGI_SetMember sm " +
+                                    "where d._SetMember_key = sm._SetMember_key");
+        }
+        catch (MGIException e) {
+          SNPLoaderException e1 =
+                  (SNPLoaderException) snpEFactory.getException(
+                  SNPLoaderExceptionFactory.SNPDeleteStrainSetErr, e);
+              throw e1;
+      }
     }
 }
