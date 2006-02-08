0a177fa1728d0e23fc761f88f3207a361c59c38c
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-02-08 15:01:56 +0000

    updated VARCLASS_NAMED and VARCLASS_MNP

--- java/org/jax/mgi/app/dbsnploader/SNPLoaderConstants.java
+++ java/org/jax/mgi/app/dbsnploader/SNPLoaderConstants.java
@@ -49,12 +49,10 @@ public class SNPLoaderConstants {
         // orientation values from NSE file
         public static final String NSE_FORWARD = "forward";
         public static final String NSE_REVERSE = "reverse";
-        //public static final String NSE_SS_REVERSE = "reverse";
-        //public static final String NSE_RS_REVERSE = "reverse";
         // variation class constants
-        public static final String VARCLASS_NAMED = "named";
+        public static final String VARCLASS_NAMED = "named-locus";
         public static final String VARCLASS_INDEL = "in-del";
         public static final String VARCLASS_SNP = "snp";
-        public static final String VARCLASS_MNP = "mnp";
+        public static final String VARCLASS_MNP = "multinucleotide-polymorphism";
         public static final String VARCLASS_MIXED = "mixed";
     }
