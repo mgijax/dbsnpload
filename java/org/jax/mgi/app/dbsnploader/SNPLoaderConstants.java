26275a5f3fffb0d89602c8b9f1cefc93f03947e5
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-03-02 12:47:26 +0000

    removed Object type values, Ldb values, added MGI_Organism value for mouse, removed fxn class value

--- java/org/jax/mgi/app/dbsnploader/SNPLoaderConstants.java
+++ java/org/jax/mgi/app/dbsnploader/SNPLoaderConstants.java
@@ -21,34 +21,22 @@ public class SNPLoaderConstants {
 * @author sc
 */
 
-
-        // Object type values
-        public static final String OBJECTYPE_CSNP = "Consensus SNP";
-        public static final String OBJECTYPE_SSNP = "Sub SNP";
-        //public static final String OBJECTYPE_SMARKER = "MGI_SNP_Marker";
-
         // accession id prefixes
         public static final String PREFIX_CSNP = "rs";
         public static final String PREFIX_SSNP = "ss";
 
-        // LogicalDB values
-        public static final String LDB_CSNP = "RefSNP";
-        public static final String LDB_SSNP = "SubSNP";
-        public static final String LDB_SUBMITTER = "SubmitterSNP";
-        public static final String LDB_REFSEQ = "RefSeq";
-
         // Assemblies as found in dbSNP
+        public static final String MGI_MOUSE = "mouse, laboratory";
         public static final String DBSNP_BL6 = "C57BL/6J";
-
-        // dbSNP fxn classes
-        public static final String REFERENCE = "reference";
 
         // orientation values from genotype file
         public static final String GENO_FORWARD_ORIENT = "fwd";
         public static final String GENO_REVERSE_ORIENT = "rev";
+
         // orientation values from NSE file
         public static final String NSE_FORWARD = "forward";
         public static final String NSE_REVERSE = "reverse";
+
         // variation class constants
         public static final String VARCLASS_NAMED = "named-locus";
         public static final String VARCLASS_INDEL = "in-del";
