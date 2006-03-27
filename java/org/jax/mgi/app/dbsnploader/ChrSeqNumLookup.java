aa8b2103c9991c64798e6fd47b6f30dfe72bf439
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-03-27 19:34:09 +0000

    initial commit

---
+++ java/org/jax/mgi/app/dbsnploader/ChrSeqNumLookup.java
@@ -0,0 +1,89 @@
+package org.jax.mgi.app.dbsnploader;
+
+import org.jax.mgi.dbs.SchemaConstants;
+import org.jax.mgi.shr.cache.CacheException;
+import org.jax.mgi.shr.cache.FullCachedLookup;
+import org.jax.mgi.shr.cache.KeyNotFoundException;
+import org.jax.mgi.shr.cache.KeyValue;
+import org.jax.mgi.shr.config.ConfigException;
+import org.jax.mgi.shr.dbutils.DBException;
+import org.jax.mgi.shr.dbutils.RowDataInterpreter;
+import org.jax.mgi.shr.dbutils.RowReference;
+import org.jax.mgi.shr.dbutils.SQLDataManagerFactory;
+import org.jax.mgi.dbs.mgd.LogicalDBConstants;
+import org.jax.mgi.dbs.mgd.MGITypeConstants;
+
+import java.lang.Integer;
+
+
+/**
+ * @is An object that the sequenceNum of a chromosome given a _Chromosome_Key
+ * @has A query to get _Chromosome_Key/sequenceNum pairs
+ * @does Provides a method to look up sequenceNum given a _Chromosome_Key
+ * @company The Jackson Laboratory
+ * @author sc
+ */
+
+public class ChrSeqNumLookup extends FullCachedLookup
+{
+    /**
+     * Constructs a ChromosomeSeqNumLookup object.
+     * @assumes Nothing
+     * @effects Nothing
+     * @throws CacheException thrown if there is an error accessing the cache
+     * @throws ConfigException thrown if there is an error accessing the
+     * configuration
+     * @throws DBException thrown if there is an error accessing the
+     * database
+     */
+    public ChrSeqNumLookup ()
+        throws CacheException, ConfigException, DBException {
+        super(SQLDataManagerFactory.getShared(SchemaConstants.MGD));
+    }
+
+
+    /**
+     * Looks Population Id to find its Handle name.
+     * @assumes Nothing
+     * @effects Nothing
+     * @param popId Population Id.
+     * @return A String object containing the Handle name for popId
+     * @throws CacheException thrown if there is an error accessing the cache
+     * @throws DBException thrown if there is an error accessing the
+     * database
+     * @throws KeyNotFoundException thrown if the key is not found
+     */
+    public Integer lookup (Integer key)
+        throws KeyNotFoundException, DBException, CacheException {
+        return (Integer)super.lookup(key);
+        //return new Integer((String)super.lookup(key));
+    }
+
+    /**
+     * Get the query to fully initialize the cache.
+     * @assumes Nothing
+     * @effects Nothing
+     * @return The query to fully initialize the cache.
+     */
+    public String getFullInitQuery () {
+        return new String("SELECT _Chromosome_Key, sequenceNum " +
+                          "FROM MRK_Chromosome");
+    }
+
+    /**
+     * Get a RowDataInterpreter for creating a KeyValue object from a database
+     * used for creating a new cache entry.
+     * @assumes nothing
+     * @effects nothing
+     * @return The RowDataInterpreter object.
+     */
+    public RowDataInterpreter getRowDataInterpreter() {
+        class Interpreter implements RowDataInterpreter {
+            public Object interpret (RowReference row)
+                throws DBException {
+                return new KeyValue(row.getInt(1), row.getInt(2));
+            }
+        }
+        return new Interpreter();
+    }
+}
