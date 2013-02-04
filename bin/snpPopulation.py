35bf6dcec5396fc2eba9f3d3ddd849c6885afaff
MGI Admin <mgiadmin@jax.org>
2013-02-04 13:13:38 +0000

    TR11248

--- bin/snpPopulation.py
+++ bin/snpPopulation.py
@@ -82,29 +82,42 @@ def setup():
 		handleKeyLookup[r[1]] = r[0]
 	print handleKeyLookup
 
-    	#print 'Creating SNP_Accession max key'
-	#sys.stdout.flush()
-	#results = db.sql('''SELECT max(_Accession_key) as maxKey FROM SNP_Accession''', 'auto')
-	#accKey = results[1][0][0]
+	print 'Delete from SNP_Accession...'
+	sys.stdout.flush()
+	db.sql('delete from SNP_Accession where _mgitype_key = 33', None)
+	db.commit()
+
+    	print 'Creating SNP_Accession max key'
+	sys.stdout.flush()
+	results = db.sql('''SELECT max(_Accession_key) as maxKey FROM SNP_Accession''', 'auto')
+	accKey = results[1][0][0]
 
 def createBCP():
+
 	print 'Creating %s/%s.bcp' % (outputdir, popTable)
 	print 'and  %s/%s.bcp' % (outputdir, accTable)
+
 	inFile = open(os.environ['POP_FILE'], 'r')
+
 	primaryKey = 0
 	problemHandles = ''
 	line = string.strip(inFile.readline())
+
 	# line looks like:
 	# <Population popId="1064" handle="ROCHEBIO" locPopId="RPAMM">
+
 	while line:
+
 	    # remove <>
 	    line = line[1:-1]
 	    line = line.replace('Acadl SNP variants', 'Acadl_SNP_variants')
 	    tokens = string.split(line)
+
 	    popName = ''
 	    handle = ''
 	    popId = ''
 	    handleKey = ''
+
 	    for token in tokens:
 		if string.find(token, '=') != -1:
 			attrs = string.split(token, '=')
@@ -118,33 +131,44 @@ def createBCP():
                             handle = value
 			elif key == 'locPopId':
 			    popName = value
+
 	    if handleKeyLookup.has_key(handle):
     	        handleKey = handleKeyLookup[handle]
+
 	    if popName == '' or handle == '' or popId == '' or handleKey == '':
 		problemHandles = '%s%s%s' % (problemHandles, NL, line)
+
 	    primaryKey = primaryKey + 1
-	    bcpLine = str(primaryKey) + DL + \
-		str(handle) + DL + \
-		str(handleKey) + DL + \
+
+	    bcpLine = str(primaryKey) + TAB + \
+		str(handle) + TAB + \
+		str(handleKey) + TAB + \
                 str(popName) + NL
+
 	    popBCP.write(bcpLine)
+
 	    createAccession(popId, primaryKey, snpPopLdbKey, snpPopmgiTypeKey) 
+
 	    line = string.strip(inFile.readline())
 
 	popBCP.close()
 	accBCP.close()
+
 	if problemHandles != '':
 	    sys.exit('Problem Handles %s' % problemHandles)
+
 def createAccession(accid, objectKey, ldbKey, mgiTypeKey):
     global accKey
+
     accKey = accKey + 1
     prefixpart, numericpart = accessionlib.split_accnum(accid)
-    accBCP.write(str(accKey) + DL + \
-	str(accid) + DL + \
-	str(prefixpart) + DL + \
-	str(numericpart) + DL + \
-	str(ldbKey) + DL + \
-	str(objectKey) + DL + \
+
+    accBCP.write(str(accKey) + TAB + \
+	str(accid) + TAB + \
+	str(prefixpart) + TAB + \
+	str(numericpart) + TAB + \
+	str(ldbKey) + TAB + \
+	str(objectKey) + TAB + \
 	str(mgiTypeKey) + NL)
 #
 # Main Routine
@@ -152,6 +176,6 @@ def createAccession(accid, objectKey, ldbKey, mgiTypeKey):
 
 print '%s' % mgi_utils.date()
 setup()
-#createBCP()
+createBCP()
 print '%s' % mgi_utils.date()
 
