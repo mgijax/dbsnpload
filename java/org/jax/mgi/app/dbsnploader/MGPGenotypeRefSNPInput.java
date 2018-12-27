e05ee1388f70d9416ccf2295edbf29e3962f1352
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2018-12-27 07:28:41 -0500

    MGP specific version of DBSNPGenotypeRefSNPInput

---
+++ java/org/jax/mgi/app/dbsnploader/MGPGenotypeRefSNPInput.java
@@ -0,0 +1,50 @@
+package org.jax.mgi.app.dbsnploader;
+
+/**
+ *
+ * is a data object representing the populations, by SubSnp, for a RefSnp
+ * @has an rs id and a mapping of ss id to its population(s)
+ * @company Jackson Laboratory
+ * @author sc
+ *
+ */
+
+class MGPGenotypeRefSNPInput {
+
+    // mapping looks like: {strain:AlleleObject, ...}
+	// only one population for mgp
+    //private HashMap ssPopulationMap;
+
+    // RefSnp id
+    private int rsId;
+    
+    // pipe-delim string for strain alleles for this rs
+    private String strainAlleles;
+   
+
+    /**
+     * constructor
+     */
+    public MGPGenotypeRefSNPInput(int rs, String sa) {
+        rsId = rs;
+        strainAlleles = sa;
+    }
+
+    /**
+     * gets the rs id
+     * @return the rs id
+     */
+    public int getRsId() {
+        return rsId;
+    }
+    
+    /**
+     * gets strain/allele map
+     * @return HashMap of strain/alleles
+     */
+    public String getStrainAlleles() {
+        return strainAlleles;
+    }
+
+}
+
